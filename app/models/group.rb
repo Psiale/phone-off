735cccbdfd41cb6399bb5c6e075aadb7e06419dc
psialedev <psialedev@gmail.com>
2020-06-15 12:08:08 -0500

    added active storage to group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
     belongs_to :user
     has_many :record_groups
+    has_one_attached :icon
     validates :name, presence: true
     validates :icon, presence: true
     has_many :records, through: :record_groups
--- db/migrate/20200610152906_create_groups.rb
+++ db/migrate/20200610152906_create_groups.rb
@@ -2,7 +2,6 @@ class CreateGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :groups do |t|
       t.string :name
-      t.string :icon
 
       t.timestamps
     end
